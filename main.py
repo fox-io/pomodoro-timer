86b89e59392da9935f94be6428e2a6ec6892e322
fox-io <gitlab.fox-io@foxdata.io>
2021-07-22 03:23:26 -0500

    Add main.py stub content with comment describing Pomodoro steps.

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+1. Decide on the task to be done.
+2. Set the timer to 25 minutes.
+3. Work on task until timer ends.
+4. Take 5 minute break.
+5. Repeat 1-4 4 times.
+6. Take 15-30 minute break.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
