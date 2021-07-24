c670a358306e3f7a96277df799cad7c13d9d7a04
fox-io <gitlab.fox-io@foxdata.io>
2021-07-23 19:21:52 -0500

    Basic functionality worked out.

--- main.py
+++ main.py
@@ -6,10 +6,22 @@
 5. Repeat 1-4 4 times.
 6. Take 15-30 minute break.
 """
+import time
+
+
+def start_timer(minutes, label):
+    seconds = minutes * 60
+    while seconds > 0:
+        print(f"{label} for {minutes} minutes. {seconds}")
+        time.sleep(1)
+        seconds -= 1
 
 
 def main():
-    pass
+    for round_num in range(1, 4):
+        start_timer(25, "Work")
+        start_timer(5, "Break")
+    start_timer(15, "Break")
 
 
 if __name__ == "__main__":
